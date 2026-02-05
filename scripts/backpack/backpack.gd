bf39c55c9a465e6abaf4895a77b0bd8372bff93c
TechAoba <TechAoba@163.com>
2026-02-05 17:36:52 +0800

    新增升级背包

--- scripts/backpack/backpack.gd
+++ scripts/backpack/backpack.gd
@@ -3,28 +3,26 @@ class_name Backpack extends PanelContainer
 @onready var item_grid: ItemGrid = %ItemGrid
 @export var items: Array[ItemBase] = []
 
-const SAVE_PATH = "res://backpack_save.tres"
+const SAVE_PATH = "user://backpack_save.tres"
 const backpack_item_scene: PackedScene = preload("res://scenes/backpack_item.tscn")
 
 ## 物品实例数组
 var backpack_items: Array[Node] = []
-var save_res: BackpackSaveData
-var backpack_level: int = 1
+var data: BackpackData
 
 
 func _ready() -> void:
-	save_res = BackpackSaveData.new()
+	data = BackpackData.new()
 	# 通过背包等级设置格子数量
-	var backpack_size: Vector2i = Global.get_backpack_size(backpack_level)
+	var backpack_size: Vector2i = Global.get_backpack_size(data.level)
 	item_grid.init_slot_data(backpack_size)
 	load_()
 
 
 ## 保存数据
 func save_() -> bool:
-	save_res.grid_level = self.backpack_level
-	save_res.item_datas = self.items
-	var save_status := ResourceSaver.save(save_res, SAVE_PATH)
+	data.item_datas = self.items
+	var save_status := ResourceSaver.save(data, SAVE_PATH)
 	if save_status == OK:
 		print("游戏存档保存成功: ", SAVE_PATH)
 	else:
@@ -37,16 +35,15 @@ func load_() -> void:
 	if not FileAccess.file_exists(SAVE_PATH):
 		print("游戏存档不存在： ", SAVE_PATH)
 		return
-	save_res = ResourceLoader.load(SAVE_PATH).duplicate(true) as BackpackSaveData
-	if save_res:
-		if save_res is BackpackSaveData:
+	data = ResourceLoader.load(SAVE_PATH).duplicate(true) as BackpackData
+	if data:
+		if data is BackpackData:
 			init_backpack_()
 			print("游戏存档加载成功")
 		else:
 			printerr("游戏存档格式错误！")
 	else:
 		print("游戏存档加载失败！")
-
 
 
 func add_item(item_data: ItemBase) -> void:
@@ -62,7 +59,7 @@ func add_item(item_data: ItemBase) -> void:
 ## 初始化背包
 func init_backpack_() -> void:
 	clear_()		# 清除背包数据
-	items = save_res.item_datas
+	items = data.item_datas
 	for item in self.items:
 		add_item(item)
 
@@ -73,7 +70,7 @@ func clear_() -> void:
 		backpack_item.queue_free()
 	backpack_items.clear()
 	# 清除背包格子
-	item_grid.slot_datas.fill(null)
+	item_grid.init_slot_data(Global.get_backpack_size(data.level))
 	items.clear()
 
 
@@ -92,6 +89,28 @@ func pickup_item_() -> void:
 	else:
 		items.append(item_data)
 
+
 ## 升级背包
-#func upgrade_() -> void:
-	#if 
+func upgrade_() -> void:
+	if data.level >= Global.max_backpack_level - 1:
+		print("背包已经满级")
+		return
+	
+	# 1. 升级背包
+	var old_dimention: Vector2i = Global.get_backpack_size(data.level)
+	data.level += 1
+	var new_dimention: Vector2i = Global.get_backpack_size(data.level)
+	
+	# 2. 物品的位置不会发生变化，仅修改物品的slot_idx
+	for i in items.size():
+		var x: int = items[i].in_backpack_attr.slot_idx % old_dimention.x
+		var y: int = items[i].in_backpack_attr.slot_idx / old_dimention.x
+		var new_idx: int = y * new_dimention.x + x
+		items[i].in_backpack_attr.slot_idx = new_idx
+	
+	# 3. 更新背包格子的dimentions
+	item_grid.init_slot_data(new_dimention)
+	
+	# 4. 将物品放置背包
+	for backpack_item in backpack_items:
+		var grid_index: int = item_grid.attempt_to_add_item_data(backpack_item)
--- scripts/item/backpack_data.gd
+++ scripts/item/backpack_data.gd
@@ -1,7 +1,7 @@
-class_name BackpackSaveData
+class_name BackpackData
 extends Resource
 
 # 背包等级
-@export var grid_level: int = 0
+@export var level: int = 0
 # ItemData数组（存储所有背包中的物品数据）
 @export var item_datas: Array[ItemBase] = []
